bf3c0d2f0eeb579ca6f4c6e968b06dee06bb7236
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2019-08-13 20:19:34 +0100

    fix

--- initializer.sh
+++ initializer.sh
@@ -1,13 +1,16 @@
 
 clear
 echo ''
-echo -e '\e[33m----------------------------------------------------\e[0m'
+echo '> Project name? '
+read PROJECT_NAME
+echo '> Root path? (example: /c/opt/...)'
+read ROOT PATH
 echo ''
-echo -e ' \e[32mInit hectorjs...\e[0m'
-echo ''
+echo -e '\e[33m----------------------------------------------------\e[0m\n'
+echo -e ' \e[32mInit hectorjs ...\e[0m\n'
 echo -e '   \e[36m-> Project name:\e[0m '$PROJECT_NAME
-echo -e '   \e[36m-> Root path:\e[0m    '$ROOT_PATH
-echo ''
+echo -e '   \e[36m-> Root path:\e[0m    '$ROOT_PATH'\n'
+
 cd $ROOT_PATH
 mkdir $PROJECT_NAME 
 cd $PROJECT_NAME 
@@ -26,12 +29,7 @@ echo -e '\x1b[34m| | | ||  ___/  __ \\_   _|  _  | ___ \\ |_  /  ___|\x1b[0m'
 echo -e '\x1b[34m| |_| || |__ | /  \\/ | | | | | | |_/ /   | \\ `--. \x1b[0m'
 echo -e '\x1b[34m|  _  ||  __|| |     | | | | | |    /    | |`--. \\\x1b[0m'
 echo -e '\x1b[34m| | | || |___| \\__/\\ | | \\ \\_/ / |\\ \\ \\__/ /\\__/ /\x1b[0m'
-echo -e '\x1b[34m\\_| |_/\\____/ \\____/ \\_/  \\___/\\_| \\_\\____/\____/\x1b[0m'
-echo ''
-echo -e '     \e[32mSuccessfully created :)\e[0m'
-echo ''
-echo ''
-echo -e '\e[33m----------------------------------------------------\e[0m'
-echo ''
-echo ''
+echo -e '\x1b[34m\\_| |_/\\____/ \\____/ \\_/  \\___/\\_| \\_\\____/\____/\x1b[0m \n'
+echo -e '     \e[32mSuccessfully created :)\e[0m\n\n'
+echo -e '\e[33m----------------------------------------------------\e[0m\n\n'
 
