18822c4c479688dcde7a5a555355aab6b3b0cdeb
spectrum <spectrum@santander.co.uk>
2019-10-17 20:07:36 +0100

    make the script compatible

--- initializer.sh
+++ initializer.sh
@@ -3,34 +3,59 @@
 
 clear
 echo ''
+
+
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+echo 'Machine: '${machine}
+
+case "${machine}" in
+    Linux)                 disp='\x1B';;
+    Mac)                   disp='\x1B';;
+    Cygwin)                disp='\x1B';;
+    MinGw)                 disp='\e';;
+    "UNKNOWN:${unameOut}") disp='\x1B'
+esac
+
+echo ''
+
+
 while [[ -z "$PROJECT_NAME" ]];do
- echo -e '\e[31m>\e[0m Project name? '
+ echo  ${disp}'[31m>'${disp}'[0m Project name? '
  read PROJECT_NAME
  if [[ -z "$PROJECT_NAME" ]]
  then
-      echo -e '\e[31m You must add a value  :(\e[0m\n'
+      echo  ${disp}'[31m You must add a value  :('${disp}'[0m\n'
  else
-      echo -e '\e[32m Well done  :)\e[0m\n'
+      echo  ${disp}'[32m Well done  :)'${disp}'[0m\n'
  fi
 done
 
 while [[ -z "$ROOT_PATH" ]];do
- echo -e '\e[31m>\e[0m Root path? (example: /c/opt/...) '
+ echo  ${disp}'[31m>'${disp}'[0m Root path? (example: /c/opt/...) '
  read ROOT_PATH
 
  if [[ -z "$ROOT_PATH" ]]
  then
-      echo -e '\e[31m You must add a value  :(\e[0m\n'
+      echo  ${disp}'[31m You must add a value  :('${disp}'[0m\n'
  else
-      echo -e '\e[32m Well done  :)\e[0m\n'
+      echo  ${disp}'[32m Well done  :)'${disp}'[0m\n'
  fi
 done
 
 echo ''
-echo -e '\e[33m----------------------------------------------------\e[0m\n'
-echo -e ' \e[32mInit hectorjs ...\e[0m\n'
-echo -e '   \e[36m-> Project name:\e[0m '$PROJECT_NAME
-echo -e '   \e[36m-> Root path:\e[0m    '$ROOT_PATH'\n'
+echo  ${disp}'[33m----------------------------------------------------'${disp}'[0m\n'
+echo  ' '${disp}'[32mInit hectorjs ...'${disp}'[0m\n'
+echo  '   '${disp}'[36m-> Project name:'${disp}'[0m '$PROJECT_NAME
+echo  '   '${disp}'[36m-> Root path:'${disp}'[0m    '$ROOT_PATH'\n'
 
 cd $ROOT_PATH
 mkdir $PROJECT_NAME 
@@ -45,12 +70,12 @@ npm init
 npm i @hectorjs/stub-backend
 
 echo ''
-echo -e '\x1b[34m _   _  _____ _____ _____ ___________   ___ _____\x1b[0m'
-echo -e '\x1b[34m| | | ||  ___/  __ \\_   _|  _  | ___ \\ |_  /  ___|\x1b[0m'
-echo -e '\x1b[34m| |_| || |__ | /  \\/ | | | | | | |_/ /   | \\ `--. \x1b[0m'
-echo -e '\x1b[34m|  _  ||  __|| |     | | | | | |    /    | |`--. \\\x1b[0m'
-echo -e '\x1b[34m| | | || |___| \\__/\\ | | \\ \\_/ / |\\ \\ \\__/ /\\__/ /\x1b[0m'
-echo -e '\x1b[34m\\_| |_/\\____/ \\____/ \\_/  \\___/\\_| \\_\\____/\____/\x1b[0m \n'
-echo -e '     \e[32mSuccessfully created :)\e[0m\n\n'
-echo -e '\e[33m----------------------------------------------------\e[0m\n\n'
+echo  '\x1b[34m _   _  _____ _____ _____ ___________   ___ _____\x1b[0m'
+echo  '\x1b[34m| | | ||  ___/  __ \\_   _|  _  | ___ \\ |_  /  ___|\x1b[0m'
+echo  '\x1b[34m| |_| || |__ | /  \\/ | | | | | | |_/ /   | \\ `--. \x1b[0m'
+echo  '\x1b[34m|  _  ||  __|| |     | | | | | |    /    | |`--. \\\x1b[0m'
+echo  '\x1b[34m| | | || |___| \\__/\\ | | \\ \\_/ / |\\ \\ \\__/ /\\__/ /\x1b[0m'
+echo  '\x1b[34m\\_| |_/\\____/ \\____/ \\_/  \\___/\\_| \\_\\____/\____/\x1b[0m \n'
+echo  '     '${disp}'[32mSuccessfully created :)'${disp}'[0m\n\n'
+echo  ${disp}'[33m----------------------------------------------------'${disp}'[0m\n\n'
 
