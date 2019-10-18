4d2ea8c4d5fb30c48269f0b4e95afb25349c09af
spectrum <spectrum@santander.co.uk>
2019-10-18 08:08:39 +0100

    make unique sh for mac and windows

--- initializer.sh
+++ initializer.sh
@@ -2,9 +2,7 @@
 ## Execute this shell script in order to create a default stub project
 
 clear
-echo ''
-
-
+printf ''
 
 unameOut="$(uname -s)"
 case "${unameOut}" in
@@ -15,8 +13,6 @@ case "${unameOut}" in
     *)          machine="UNKNOWN:${unameOut}"
 esac
 
-echo 'Machine: '${machine}
-
 case "${machine}" in
     Linux)                 disp='\x1B';;
     Mac)                   disp='\x1B';;
@@ -25,57 +21,56 @@ case "${machine}" in
     "UNKNOWN:${unameOut}") disp='\x1B'
 esac
 
-echo ''
-
+printf  '\n'${disp}'[33mMachine: '$machine' '${disp}'[0m\n\n'
 
 while [[ -z "$PROJECT_NAME" ]];do
- echo  ${disp}'[31m>'${disp}'[0m Project name? '
+ printf ${disp}'[31m>'${disp}'[0m Project name? \n'
  read PROJECT_NAME
  if [[ -z "$PROJECT_NAME" ]]
  then
-      echo  ${disp}'[31m You must add a value  :('${disp}'[0m\n'
+      printf  ${disp}'[31m You must add a value  :('${disp}'[0m\n\n'
  else
-      echo  ${disp}'[32m Well done  :)'${disp}'[0m\n'
+      printf  '\n'${disp}'[32m Well done  :)'${disp}'[0m\n\n'
  fi
 done
 
 while [[ -z "$ROOT_PATH" ]];do
- echo  ${disp}'[31m>'${disp}'[0m Root path? (example: /c/opt/...) '
+ printf  ${disp}'[31m>'${disp}'[0m Root path? (example: /c/opt/...) \n\n'
  read ROOT_PATH
 
  if [[ -z "$ROOT_PATH" ]]
  then
-      echo  ${disp}'[31m You must add a value  :('${disp}'[0m\n'
+      printf  ${disp}'[31m You must add a value  :('${disp}'[0m\n\n'
  else
-      echo  ${disp}'[32m Well done  :)'${disp}'[0m\n'
+      printf  '\n'${disp}'[32m Well done  :)'${disp}'[0m\n\n'
  fi
 done
 
-echo ''
-echo  ${disp}'[33m----------------------------------------------------'${disp}'[0m\n'
-echo  ' '${disp}'[32mInit hectorjs ...'${disp}'[0m\n'
-echo  '   '${disp}'[36m-> Project name:'${disp}'[0m '$PROJECT_NAME
-echo  '   '${disp}'[36m-> Root path:'${disp}'[0m    '$ROOT_PATH'\n'
+printf  '\n'${disp}'[33m----------------------------------------------------'${disp}'[0m\n\n'
+printf  ' '${disp}'[32mInit hectorjs ...'${disp}'[0m\n\n'
+printf  '   '${disp}'[36m-> Project name:'${disp}'[0m '$PROJECT_NAME'\n'
+printf  '   '${disp}'[36m-> Root path:'${disp}'[0m    '$ROOT_PATH'\n\n'
+printf  '\n'${disp}'[33m- - - - - - - - - - - - - - - - - - - - - - - - - - '${disp}'[0m\n\n'
 
 cd $ROOT_PATH
 mkdir $PROJECT_NAME 
 cd $PROJECT_NAME 
-echo "require('@hectorjs/stub-backend')" > app.js
+printf "require('@hectorjs/stub-backend')" > app.js
 mkdir resources 
 cd resources
-echo {} > temp.json
+printf {} > temp.json
 cd ..
 code .
 npm init
-npm i @hectorjs/stub-backend
+npm install @hectorjs/stub-backend
 
-echo ''
-echo  '\x1b[34m _   _  _____ _____ _____ ___________   ___ _____\x1b[0m'
-echo  '\x1b[34m| | | ||  ___/  __ \\_   _|  _  | ___ \\ |_  /  ___|\x1b[0m'
-echo  '\x1b[34m| |_| || |__ | /  \\/ | | | | | | |_/ /   | \\ `--. \x1b[0m'
-echo  '\x1b[34m|  _  ||  __|| |     | | | | | |    /    | |`--. \\\x1b[0m'
-echo  '\x1b[34m| | | || |___| \\__/\\ | | \\ \\_/ / |\\ \\ \\__/ /\\__/ /\x1b[0m'
-echo  '\x1b[34m\\_| |_/\\____/ \\____/ \\_/  \\___/\\_| \\_\\____/\____/\x1b[0m \n'
-echo  '     '${disp}'[32mSuccessfully created :)'${disp}'[0m\n\n'
-echo  ${disp}'[33m----------------------------------------------------'${disp}'[0m\n\n'
+printf ''
+printf  '\x1b[34m _   _  _____ _____ _____ ___________   ___ _____\x1b[0m\n'
+printf  '\x1b[34m| | | ||  ___/  __ \\_   _|  _  | ___ \\ |_  /  ___|\x1b[0m\n'
+printf  '\x1b[34m| |_| || |__ | /  \\/ | | | | | | |_/ /   | \\ `--. \x1b[0m\n'
+printf  '\x1b[34m|  _  ||  __|| |     | | | | | |    /    | |`--. \\\x1b[0m\n'
+printf  '\x1b[34m| | | || |___| \\__/\\ | | \\ \\_/ / |\\ \\ \\__/ /\\__/ /\x1b[0m\n'
+printf  '\x1b[34m\\_| |_/\\____/ \\____/ \\_/  \\___/\\_| \\_\\____/\____/\x1b[0m \n\n'
+printf  '     '${disp}'[32mSuccessfully created :)'${disp}'[0m\n\n\n'
+printf  ${disp}'[33m----------------------------------------------------'${disp}'[0m\n\n\n'
 
