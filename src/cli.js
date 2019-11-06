378aaa18040bff434fe7657dad6bbadd58e8e26b
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2019-11-06 19:25:32 +0000

    improve logs

--- src/cli.js
+++ src/cli.js
@@ -1,41 +1,45 @@
 import { createInterface } from 'readline';
 import { cd, exec, mkdir, touch } from 'shelljs';
 import { writeFile } from 'fs';
+import { info, warn } from 'console';
 
+const VERSION = '0.31.0';
 
 export async function cli(args) {
 
     if (!args) {
-        console.log('Sorry, you missed a parameter like --new, --version..');
+        warn('\n\x1b[31m Sorry, you missed a parameter (hjs --help) \x1b[0m')
         process.exit();
     }
 
     if (args.version) {
-        console.log('\n\x1b[33mversion: 0.30.0\x1b[0m\n')
+        info(`\n\x1b[33mversion: ${VERSION}\x1b[0m\n`)
         process.exit();
     }
 
     if (args.help) {
-        console.log('\n\x1b[32mBelow, you can see different options for your mock:\n\x1b[0m\n')
-        console.log(`\x1b[32m --new     : create new mock project\x1b[0m`);
-        console.log(`\x1b[32m --version : know version hjs\x1b[0m`);
-        console.log(`\x1b[32m --vs      : open visual code studio if exists\x1b[0m`);
-        console.log(`\x1b[32m --licence : MIT\n\n\x1b[0m`);
-        console.log(`\x1b[32mExample: hjs --new --vs\n\x1b[0m`);
-        console.log('\x1b[33mversion: 0.30.0\x1b[0m\n')
+        info('\n\x1b[32mBelow, you can see different options for your mock:\n\x1b[0m\n')
+        info(`\x1b[32m --new     : create new mock project\x1b[0m`);
+        info(`\x1b[32m --version : know version hjs\x1b[0m`);
+        info(`\x1b[32m --vs      : open visual code studio if exists\x1b[0m`);
+        info(`\x1b[32m --licence : MIT\n\n\x1b[0m`);
+        info(`\x1b[32mExample: hjs --new --vs\n\x1b[0m`);
+        info(`\x1b[33mversion: ${VERSION}\x1b[0m\n`)
         process.exit();
     }
 
     if (args.new) {
+        var start = new Date()
+
         exec('clear');
-        
+
         var nameProject = await handleQuestion('Project name?').catch(() => process.exit());
         var pathProject = await handleQuestion('Root path? (example: /c/opt/...)').catch(() => process.exit());
-        console.log(`\n\x1b[33m----------------------------------------------------\x1b[0m\n`);
-        console.log(`\x1b[32m Init hectorjs ...\x1b[0m\n`);
-        console.log(`\x1b[32m -> Project name: ${nameProject}\x1b[0m`);
-        console.log(`\x1b[32m -> Root path: ${pathProject}\x1b[0m\n`);
-        console.log(`\x1b[33m- - - - - - - - - - - - - - - - - - - - - - - - - - \x1b[0m\n`);
+        info(`\n\x1b[33m----------------------------------------------------\x1b[0m\n`);
+        info(`\x1b[32m Init hectorjs ...\x1b[0m\n`);
+        info(`\x1b[32m -> Project name: ${nameProject}\x1b[0m`);
+        info(`\x1b[32m -> Root path: ${pathProject}\x1b[0m\n`);
+        info(`\x1b[33m- - - - - - - - - - - - - - - - - - - - - - - - - - \x1b[0m\n`);
 
         cd(pathProject);
         mkdir(nameProject);
@@ -58,7 +62,7 @@ export async function cli(args) {
             if (err) throw err;
         });
 
-        console.log('\x1b[32m Be aware if you dont have hjs set globally, you will need to run the server via node command\x1b[0m\n')
+        info('\x1b[32m Be aware if you dont have hjs set globally, you will need to run the server via node command\x1b[0m\n')
         var runHealth = await handleQuestion('Run service with health check? (y/N)').catch(() => process.exit());
         if (args['vs'] && args['vs'] == true) {
             exec('code .');
@@ -66,12 +70,15 @@ export async function cli(args) {
         if (runHealth == 'y') {
             require('@hectorjs/stub-backend');
         } else {
-            console.log('Ready to test (run node app.js)');
+            info('Ready to test (run node app.js)');
         }
+
+        var end = new Date() - start
+        info('\n\x1b[32mExecution time: %dms \x1b[0m', end)
         process.exit();
     }
 
-    console.log('Sorry, you missed a parameter like --new, --version..');
+    warn('\n\x1b[31m Sorry, you missed a parameter (hjs --help) \x1b[0m')
     process.exit();
 };
 
@@ -89,11 +96,11 @@ function handleQuestion(message) {
 
 function handleAnswer(resolve, reject, readline, value) {
     if (!value) {
-        console.log(`\x1b[31m You must add a value  :(\x1b[0m`);
+        info(`\x1b[31m You must add a value  :(\x1b[0m`);
         readline.close();
         reject();
     } else {
-        console.log(`\x1b[32m Well done  :)\x1b[0m`)
+        info(`\x1b[32m Well done  :)\x1b[0m`)
         readline.close();
         resolve(value);
     }
