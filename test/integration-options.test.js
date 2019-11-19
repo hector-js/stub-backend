5c1f771ef42fd54e3a9906a5f34147efd7aff28f
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2019-11-19 19:37:36 +0000

    eslint(project): apply eslint rules

--- test/integration-options.test.js
+++ test/integration-options.test.js
@@ -9,12 +9,12 @@ const expect = chai.expect;
 describe('OPTIONS - stub backend project', () => {
   it('returns a 200 status', (done) => {
     request(app)
-      .options('/story/nathan')
-      .end((err, res) => {
-        expect(err).to.not.exist;
-        expect(res.status).to.equal(200);
-        expect(res.body).to.be.empty;
-        done();
-      });
+        .options('/story/nathan')
+        .end((err, res) => {
+          expect(err).to.not.exist;
+          expect(res.status).to.equal(200);
+          expect(res.body).to.be.empty;
+          done();
+        });
   });
 });
