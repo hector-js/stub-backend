f6bde7ff66881b05252e9f4535c5fb33f8fffa6b
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2019-11-23 01:55:03 +0100

    version incriesed

--- src/cli.js
+++ src/cli.js
@@ -7,7 +7,7 @@ import { start } from './start/start.cli';
 import { exec } from 'shelljs';
 
 const chalk = require('chalk');
-const VERSION = '0.57.0';
+const VERSION = '0.58.0';
 
 export function cli(args) {
     switch (args._[0]) {
