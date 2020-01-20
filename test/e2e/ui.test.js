19502bb2859a0c60152cee61fbda1e1b6f0d4ebb
Hector Jimenez <hector.jimenez.sanchez@gmail.com>
2020-01-20 18:37:25 +0000

    #29 refact(delete.1.json,delete.test.js) include test when response body does not exist

--- test/e2e/ui.test.js
+++ test/e2e/ui.test.js
@@ -28,44 +28,52 @@ describe('UI', () => {
           });
     });
 
-    it('returns an file ico when "/favicon.ico" is called', (done) => {
-      request(app)
-          .get('/favicon.ico')
-          .end((err, res) => {
-            expect(err).to.not.exist;
-            expect(res.status).to.equal(200);
-            done();
-          });
+    context(' when "/favicon.ico" is called', () => {
+      it('returns an ico file', (done) => {
+        request(app)
+            .get('/favicon.ico')
+            .end((err, res) => {
+              expect(err).to.not.exist;
+              expect(res.status).to.equal(200);
+              done();
+            });
+      });
     });
 
-    it('returns a css when "/css/main.css" is called', (done) => {
-      request(app)
-          .get('/_css/main.css')
-          .end((err, res) => {
-            expect(err).to.not.exist;
-            expect(res.status).to.equal(200);
-            done();
-          });
+    context(' when "/css/main.css" is called', () => {
+      it('returns a css file', (done) => {
+        request(app)
+            .get('/_css/main.css')
+            .end((err, res) => {
+              expect(err).to.not.exist;
+              expect(res.status).to.equal(200);
+              done();
+            });
+      });
     });
 
-    it('returns a main.js ico when "/js/main.js" is called', (done) => {
-      request(app)
-          .get('/_js/main.js')
-          .end((err, res) => {
-            expect(err).to.not.exist;
-            expect(res.status).to.equal(200);
-            done();
-          });
+    context('when "/js/main.js" is called', () => {
+      it('returns a main.js file', (done) => {
+        request(app)
+            .get('/_js/main.js')
+            .end((err, res) => {
+              expect(err).to.not.exist;
+              expect(res.status).to.equal(200);
+              done();
+            });
+      });
     });
 
-    it('returns a file ico when "/assets/Icon.png" is called', (done) => {
-      request(app)
-          .get('/_assets/Icon.png')
-          .end((err, res) => {
-            expect(err).to.not.exist;
-            expect(res.status).to.equal(200);
-            done();
-          });
+    context(' when "/assets/Icon.png" is called', () => {
+      it('returns Icon.png', (done) => {
+        request(app)
+            .get('/_assets/Icon.png')
+            .end((err, res) => {
+              expect(err).to.not.exist;
+              expect(res.status).to.equal(200);
+              done();
+            });
+      });
     });
   });
 });
